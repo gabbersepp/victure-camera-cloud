9a095f1f0dff6ecf5988f9b6263231ca6b6ea33d
Josef Biehler <contact@biehler-josef.de>
2021-01-13 20:40:08 +0100

    extend build.cmd add exclude possibility to backup image

--- build.cmd
+++ build.cmd
@@ -10,5 +10,13 @@ CD ../cam-server-memory-cleanup
 CALL docker build -t gabbersepp/cam-server-memory-cleanup .
 CALL docker push gabbersepp/cam-server-memory-cleanup
 
+CD ../cam-client/backend
+CALL docker build -t gabbersepp/cam-client-backend .
+CALL docker push gabbersepp/cam-client-backend
+
+CD ../frontend
+CALL docker build -t gabbersepp/cam-client-frontend .
+CALL docker push gabbersepp/cam-client-frontend
+
 CD ..
 CALL kubectl apply -f deployment.yaml
--- cam-server-backup/app/index.js
+++ cam-server-backup/app/index.js
@@ -1,7 +1,15 @@
 const fs = require("fs");
 
+// specify EXCLUDEn to exclude files
+// e.g. EXCLUDE0=abc.txt
+
+const env = process.env;
+const toExclude = Object.keys(env).filter(key => key.indexOf("EXCLUDE") > -1).map(key => env[key]);
+
 const files = fs.readdirSync("/app/data");
-files.forEach(file => {
+files 
+    .filter(file => !toExclude.find(te => file.indexOf(te) > -1))
+    .forEach(file => {
     const backupPath = `/app/backup/${file}`
     const dataPath = `/app/data/${file}`;
     try {
