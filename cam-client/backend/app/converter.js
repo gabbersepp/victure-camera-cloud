296da5a80e41d4b4133ded6a1ee4e13d38e4feff
Josef Biehler <contact@biehler-josef.de>
2021-01-13 19:15:56 +0100

    just a few refactorings

--- cam-client/backend/app/converter.js
+++ cam-client/backend/app/converter.js
@@ -1,6 +1,13 @@
 const fs = require("fs");
 const child_process = require("child_process");
 
+/**
+ * Does the converted file already exist?
+ * This is determined based on the filename.
+ * If a video is requested, the specified one is converted and saved under the original filename
+ * The original video gets renamed to *_original
+ * @param {*} filePath 
+ */
 async function ensureConvertedFile(filePath) {
     const extensionSplitterIndex = filePath.lastIndexOf(".");
     const extension = filePath.substr(extensionSplitterIndex + 1, 10);
@@ -8,68 +15,44 @@ async function ensureConvertedFile(filePath) {
     const originalName = `${filePath.substr(0, extensionSplitterIndex)}_original.${extension}`;
     if (!fs.existsSync(originalName)) {
         fs.renameSync(filePath, originalName)
-        // convert
-        await createProcess(filePath, originalName)
+        const cmd = `vlc --intf dummy ${originalName} :sout=#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=none}:std{access=file{no-overwrite},mux=mp4,dst='${filePath}'} vlc://quit`;
+        await createProcess(cmd)
     }
 }
 
+/**
+ * Ensure that a thumbnail exists
+ * Create a new one otherwise
+ * @param {*} videoContainingDir 
+ * @param {*} videoName 
+ * @param {*} thumbnailContainingDir 
+ */
 async function ensureThumbnail(videoContainingDir, videoName, thumbnailContainingDir) {
-    const extensionSplitterIndex = videoName.lastIndexOf(".");
-    const thumbnailName = `${videoName.substr(0, extensionSplitterIndex)}.png`;
-
     const tempDirName = `${thumbnailContainingDir}/${videoName}_dir`;
     const thumbnailPath = `${thumbnailContainingDir}/${videoName}.png`
     const videoPath = `${videoContainingDir}/${videoName}`
 
     if (!fs.existsSync(thumbnailPath)) {
-        return new Promise((resolve) => {
-            if (!fs.existsSync(tempDirName)) {
-                fs.mkdirSync(tempDirName);
-            }
+        if (!fs.existsSync(tempDirName)) {
+            fs.mkdirSync(tempDirName);
+        }
 
-            const cmd = `vlc ${videoPath} --intf dummy --video-filter=scene --start-time=0 --stop-time=1 --scene-ratio=8 --scene-path="${tempDirName}" --vout=dummy --avcodec-hw=none vlc://quit`;
-            console.log("#######\r\nexecute command:\r\n\t" + cmd)
+        // this will create multiple snapshots
+        const cmd = `vlc ${videoPath} --intf dummy --video-filter=scene --start-time=0 --stop-time=1 --scene-ratio=8 --scene-path="${tempDirName}" --vout=dummy --avcodec-hw=none vlc://quit`;
+        await createProcess(cmd);
 
-            let processEnded = false;
-            const pr =  child_process.exec(cmd);
-        
-            pr.stdout.on('data', data => {
-                console.log(data.toString()); 
-            });
-        
-            pr.stderr.on('data', data => {
-                console.log(data.toString()); 
-            });
-
-            function finish() {
-                if (!processEnded) {
-                    const firstImg = fs.readdirSync(tempDirName)[0]
-                    fs.copyFileSync(tempDirName + "/" + firstImg, thumbnailPath)
-                    fs.rmdirSync(tempDirName, { recursive: true, force: true })
-                    resolve(thumbnailPath)
-                    processEnded = true;
-                }
-            }
-        
-            pr.on("close", () => {
-                finish()
-            })
-        
-            pr.on("exit", () => {
-                finish()
-            })
-        
-            return pr;
-        })
+        // there may be multiple snapshots, take the first one
+        const firstImg = fs.readdirSync(tempDirName)[0]
+        fs.copyFileSync(tempDirName + "/" + firstImg, thumbnailPath)
+        fs.rmdirSync(tempDirName, { recursive: true, force: true })
     }
 
-    return new Promise(resolve => resolve(thumbnailPath));
+    return thumbnailPath;
 }
 
-function createProcess(filePath, originalName) {
+function createProcess(cmd) {
     return new Promise((resolve) => {
-        const cmd = `vlc --intf dummy ${originalName} :sout=#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=none}:std{access=file{no-overwrite},mux=mp4,dst='${filePath}'} vlc://quit`;
-        console.log("#######\r\nexecute command:\r\n\t" + cmd)
+       console.log("#######\r\nexecute command:\r\n\t" + cmd)
         let processEnded = false;
         const pr =  child_process.exec(cmd);
     
--- cam-client/backend/app/index.js
+++ cam-client/backend/app/index.js
@@ -10,86 +10,82 @@ const app = express();
 app.use(cors())
 
 const config = utils.getConfig();
-const path = config.path
+const dataPath = config.path
 
 app.get("/thumbnail/:video", async (req, res) => {
   if (!req.params.video) {
-    res.sendStatus(200);
+    res.sendStatus(400);
+    return;
   }
-  const thumnailPath = await converter.ensureThumbnail(path, req.params.video, path + "/thumbnail")
+
+  const thumnailPath = await converter.ensureThumbnail(dataPath, req.params.video, dataPath + "/thumbnail")
   res.sendFile(thumnailPath);
 })
 
 app.get("/list/:page", (req, res) => {
-
-    const files = fs.readdirSync(path);
-    let sorted = files.map(f => {
-      const matches = f.match(/([0-9]{4}.*)\.[0-9]{3}Z\./);
-      if (!matches || matches.length < 2) {
-        // filter out already converted videos
-        return null;
+  const files = fs.readdirSync(dataPath);
+  let sorted = files.map(f => {
+    // this ensures that *_original videos are never read
+    const matches = f.match(/([0-9]{4}.*)\.[0-9]{3}Z\./);
+    if (!matches || matches.length < 2) {
+      // filter out already converted videos
+      return null;
+    }
+      const dateStr = matches[1]
+      return [`${f}`, date.parse(dateStr, "YYYY-MM-DDTHH-mm-ss", true)]
+  })
+  .filter(x => x)
+  .sort((a, b) => b[1].getTime() - a[1].getTime())
+  .map(f => {
+      return {
+          path: f[0],
+          date: f[1]
       }
-        const dateStr = matches[1]
-        return [`${f}`, date.parse(dateStr, "YYYY-MM-DDTHH-mm-ss", true)]
-    })
-    .filter(x => x)
-    .sort((a, b) => b[1].getTime() - a[1].getTime())
-    .map(f => {
-        return {
-            path: f[0],
-            date: f[1]
-        }
-    })
+  });
 
-    const page = (req.params.page || 1) - 1;
-    sorted = sorted.slice(page * 10, page * 10 + 10)
-    res.send(sorted)
+  // used to enable endless scrolling on client
+  const page = (req.params.page || 1) - 1;
+  sorted = sorted.slice(page * 10, page * 10 + 10)
+  res.send(sorted)
 })
 
 app.get("/file/:video", async (req, res) => {
-    let url = req.url
-    /*const fileMatches = url.match(/\/file\/(.*)/)
+  if (!req.params.video) {
+    res.sendStatus(400);
+    return;
+  }
 
-    if (!fileMatches || fileMatches.length < 2) {
-        res.sendStatus(500);
-        console.error(`filename could not be parsed from ${url}`)
-        res.send("filename could not be parsed");
-    }*/
+  const videoName = req.params.video;
+  const videoPath = dataPath + "/" + videoName;
 
-    const videoName = req.params.video;//fileMatches[1];
-    const videoPath = path + "/" + videoName;
+  await converter.ensureConvertedFile(videoPath);
 
-    await converter.ensureConvertedFile(videoPath);
-
-    const stat = fs.statSync(videoPath)
-    const fileSize = stat.size
-    const range = req.headers.range
-    if (range) {
-      const parts = range.replace(/bytes=/, "").split("-")
-      const start = parseInt(parts[0], 10)
-      const end = parts[1] 
-        ? parseInt(parts[1], 10)
-        : fileSize-1
-      const chunksize = (end-start)+1
-      const file = fs.createReadStream(videoPath, {start, end})
-      const head = {
-        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-        'Accept-Ranges': 'bytes',
-        'Content-Length': chunksize,
-        'Content-Type': 'video/mp4; codecs="avc1.4d002a"',
-      }
-      res.writeHead(206, head);
-      file.pipe(res);
-    } else {
-      const head = {
-        'Content-Length': fileSize,
-        'Content-Type': 'video/mp4',
-      }
-      res.writeHead(200, head)
-      fs.createReadStream(videoPath).pipe(res)
+  const stat = fs.statSync(videoPath)
+  const fileSize = stat.size
+  const range = req.headers.range
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize-1
+    const chunksize = (end-start)+1
+    const file = fs.createReadStream(videoPath, {start, end})
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4; codecs="avc1.4d002a"'
+    }
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4'
     }
 
+    res.writeHead(200, head)
+    fs.createReadStream(videoPath).pipe(res)
+  }
 })
 
 app.listen(port, () => console.log("listen on port " + port))
-//console.log(date.parse("Garage-2021-01-11T21-54-51.759Z.avi".match(/([0-9]{4}.*)\.[0-9]{3}Z/)[1], "YYYY-MM-DDTHH-mm-ss"))
